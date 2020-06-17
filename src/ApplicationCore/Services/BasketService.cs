0cedaf7e6a8abbe01e8e259eafa7f73f53a3f8bd
Yigit Haciefendioglu <yigith1@gmail.com>
2020-06-17 22:13:16 +0300

    BasketService

---
+++ src/ApplicationCore/Services/BasketService.cs
@@ -0,0 +1,59 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using ApplicationCore.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly IAsyncRepository<Basket> _basketRepository;
+
+        public BasketService(IAsyncRepository<Basket> basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        public async Task AddItemToBasket(int basketId, int productId, decimal price, int quantity = 1)
+        {
+            var basket = await _basketRepository
+                .FirstAsync(new BasketWithItemsSpecification(basketId));
+
+            var basketItem = basket.Items.FirstOrDefault(x => x.ProductId == productId);
+
+            if (basketItem == null)
+            {
+                basket.Items.Add(new BasketItem { ProductId = productId, Quantity = quantity, UnitPrice = price });
+            }
+            else
+            {
+                basketItem.Quantity += quantity;
+            }
+
+            await _basketRepository.UpdateAsync(basket);
+        }
+
+        public async Task DeleteBasketAsync(int basketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> GetBasketItemCountAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SetQuantities(int basketId, Dictionary<string, int> quantities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task TransferBasketAsync(string anonymousId, string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ApplicationCore/Specifications/BasketWithItemsSpecification.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Specifications
+{
+    public class BasketWithItemsSpecification : BaseSpecification<Basket>
+    {
+        public BasketWithItemsSpecification(int basketId) : base(x => x.Id == basketId)
+        {
+            AddInclude(x => x.Items);
+        }
+
+        public BasketWithItemsSpecification(string buyerId) : base(x => x.BuyerId == buyerId)
+        {
+            AddInclude(x => x.Items);
+        }
+    }
+}
